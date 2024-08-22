6c72cc5b5da0c9d25d5214d5c3442a8015add3b0
Thiraphat.ch <famlacviee60@gmail.com>
2024-08-23 05:57:56 +0700

    "Update README.md and modify id type in photosRoom model from STRING(36) to INTEGER with autoIncrement"

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 เครื่องช่วยจำ
 
 1 อย่าลืมไปเปลี่ยนในเล่มรายงาน id ของ photoshost และ photosroom ไม่เอา uuidแล้ว (ยังไม่แก้)
+
+2. จะแก้ จาก extraFeatures เป็น Available in all 
--- src/db/model/photosRoom.js
+++ src/db/model/photosRoom.js
@@ -1,18 +1,17 @@
 module.exports = (sequelize, Sequelize) => {
-    const PhotosRoom = sequelize.define(
-       'photos_room',
-       {
-          id: { type: Sequelize.STRING(36), primaryKey: true, allowNull: false, field: 'id' },
-          url: { type: Sequelize.STRING(255), allowNull: false, field: 'url' },
-          roomId: { type: Sequelize.STRING(36), allowNull: false, field: 'room_id' },
-          createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, field: 'created_at' }
-       },
-       {
-          tableName: 'photos_room',
-          timestamps: false
-       }
-    );
- 
-    return PhotosRoom;
- }
- 
+   const PhotosRoom = sequelize.define(
+      'photos_room',
+      {
+         id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false, field: 'id' },
+         url: { type: Sequelize.STRING(255), allowNull: false, field: 'url' },
+         roomId: { type: Sequelize.STRING(36), allowNull: false, field: 'room_id' },
+         createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.NOW, field: 'created_at' }
+      },
+      {
+         tableName: 'photos_room',
+         timestamps: false
+      }
+   );
+
+   return PhotosRoom;
+}
