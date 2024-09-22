d4d4b4f68506a835e38e38682e7c8d25bc9cc06c
Thiraphat.ch <famlacviee60@gmail.com>
2024-09-22 17:53:48 +0700

    "Refactor history controller to handle null accommodation and adjust response data"

--- src/controllers/history-controller.js
+++ src/controllers/history-controller.js
@@ -3,23 +3,23 @@ const createError = require('../utils/createError');
 
 exports.listHistory = async (req, res, next) => {
     try {
-
         const userId = req.user.id;
         const history = await HistoryService.getBookingRequestsByUser(userId);
 
+        // Fetch accommodation by user ID
         const accommodation = await HistoryService.findAccommodationByUserId(userId);
-        
-  
 
-        const hostId = accommodation.id;
-        const historyowner = await HistoryService.getBookingRequestsByAccommodationId(hostId);
-
-     
+        // Check if accommodation exists before accessing its properties
+        let historyowner = null;
+        if (accommodation) {
+            const hostId = accommodation.id;
+            historyowner = await HistoryService.getBookingRequestsByAccommodationId(hostId);
+        }
 
         res.status(200).json({
             status: 'success',
             datarent: history,
-            dataowner: historyowner,
+            dataowner: historyowner, 
         });
     } catch (error) {
         next(error);
